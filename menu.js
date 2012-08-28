238b4bfc11e70a1707c8d9248752cfbde6970b26
Dan Silver <dannysilver3@gmail.com>
2012-08-28 11:29:36 -0500

    Menu improvements

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ $(function() {
 	$("#item-options").click(function() {
 		$("#options-panel").slideToggle();
 	});
-	$("#page").click(function() {
+	$("#page,#content").click(function() {
 		$("#options-panel").slideUp();
 	});
 });
