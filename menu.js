af0f4647cc9c25fd01faf6f57f65d1da92e7f824
Dan Silver <dannysilver3@gmail.com>
2012-09-23 12:29:26 -0500

    Removed old, innacurate comments

--- menu.js
+++ menu.js
@@ -22,16 +22,9 @@ $(function() {
 				text: 'This will reset all settings to their default values, similar to when you first installed it on your computer.  You can restore premium background purchases by logging into your Google account.<br><strong>Do you want to reset the extension?</strong>',
 				buttons: [{addClass: 'btn btn-danger', text: 'Reset', onClick: function($noty) {
 					localStorage.clear();
-					chrome.extension.sendMessage({setDefaults: true}); //save users Facebook username
+					chrome.extension.sendMessage({setDefaults: true});
 				  }},{addClass: 'btn btn-primary', text: 'Cancel',onClick:function($noty) {$noty.close();}}
 				  ]
 		});
 	});
 });
-
-/**
-		  text: 'This will reset all settings to their default values, similar to when you first installed it on your computer.  If you purchased a premium background, you can always get it back by loggin into your google account.<br>Do you want to reset the extension?',
- 
-		});
-		
-**/
